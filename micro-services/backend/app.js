b450ce5ca584f0c81594bdb973d443036cdab69e
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-03-26 10:16:14 +0530

    cors added

--- micro-services/backend/app.js
+++ micro-services/backend/app.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from "cors";
 import { urlencoded } from 'express';
 import { connectMongoDb } from './config/db.config.js'; 
 
@@ -11,6 +12,10 @@ connectMongoDb()
 app.use(express.json())
 app.use(cookieParser())
 app.use(urlencoded({ extended: true }))
+app.use(cors({
+    origin: "https://localhost:5173",
+    credentials: true, 
+}))
 
 
 // import routes 
