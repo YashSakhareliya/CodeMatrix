11c24b8cd1c764420c46373e9ef9dac5e4c7fccf
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-04-03 11:24:23 +0530

    Code-executor server created

---
+++ micro-services/code-executor/app.js
@@ -0,0 +1,6 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from "cors";
+import { urlencoded } from 'express';
